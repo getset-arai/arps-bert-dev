21324ac907744992b764c72050e5c58a835b7ddd
getset-arai <arai.k@getset.jp>
2022-08-10 10:53:02 +0900

    Ignore hidden file

--- code/utils/estimator.py
+++ code/utils/estimator.py
@@ -4,10 +4,9 @@ from lib.bert_proofreader import BertProofreader
 logging.basicConfig(level=logging.INFO)
 
 # PRETRAINED_MODEL = "cl-tohoku/bert-base-japanese-whole-word-masking"
-PRETRAINED_MODEL_PATH = "./static/model"
-PRETRAINED_TOKENIZER_PATH = "./static/tokenizer"
+PRETRAINED_MODEL_PATH = "../../model/"
 WEIGHTS_PATH = ""
-proofreader = BertProofreader(PRETRAINED_MODEL_PATH, PRETRAINED_TOKENIZER_PATH)
+proofreader = BertProofreader(PRETRAINED_MODEL_PATH, PRETRAINED_MODEL_PATH)
 
 # 気象庁は3日午前7時59分、青森県津軽地方で、線状降水帯による非常に激しい雨が同じ場所で降り続いているとして、「顕著な大雨に関する情報」を発表した。
 # a1の性能をフルに使ってカメラ有効画素数約5010万画素・最高約30コマ/秒で撮影していると、その瞬間は以外にも早くやってきます。
--- code/utils/training.py
+++ code/utils/training.py
@@ -7,10 +7,9 @@ logging.basicConfig(level=logging.INFO)
 DIR_NAME = "../../datasets/kaden-channel"
 analyze_livedoor = AnalyzeLivedoor(DIR_NAME)
 
-PRETRAINED_MODEL_PATH = "./static/model"
-PRETRAINED_TOKENIZER_PATH = "./static/tokenizer"
+PRETRAINED_MODEL_PATH = "../../model/"
 
-finetuning = BertFinetuning(PRETRAINED_MODEL_PATH, PRETRAINED_TOKENIZER_PATH, analyze_livedoor.get_data_list())
+finetuning = BertFinetuning(PRETRAINED_MODEL_PATH, PRETRAINED_MODEL_PATH, analyze_livedoor.get_data_list())
 result = finetuning.start_training()
 if result:
     finetuning.save_weights("./static/weights")
